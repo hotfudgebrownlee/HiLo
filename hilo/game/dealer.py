e08166c954d07b5c4b8a1987d16e3d1b1c5b3423
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-01-28 17:43:33 -0700

    start game

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -7,4 +7,20 @@ class Dealer:
     """
 
     def __init__(self):
-        pass
+        """Class constructor. Declares/initializes attributes.
+
+        Args:
+        """
+        self.keep_playing = True
+        self.score = 300
+        self.player = Player()
+        self.cards = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        
+    def start_game(self):
+        """
+        
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
