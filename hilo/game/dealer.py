929f14f1ba9927a746db9328c8df4ede9df5553d
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-01-29 17:56:22 -0700

    fixed gameplay

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -20,49 +20,45 @@ class Dealer:
         """
         
         """
+        idx = random.randrange(0,13)
+        self.old_card = self.cards[idx]
         while self.keep_playing:
             self.get_inputs()
-            self.do_updates()
+            self.check_hilo()
             self.do_outputs()
 
     def get_inputs(self):
         """
 
         """
-        self.get_card()
+        print(f'The card is: {self.old_card}')
         self.player.hi_or_lo()
 
-    def get_card(self):
-        """
-        """
-        idx = random.randrange(0,13)
-        print(f'The card is: {self.cards[idx]}')
-        self.old_card = self.cards[idx]
-
-    def do_updates(self):
+    def check_hilo(self):
         """
         """
         idx = random.randrange(0,13)
         print(f'Next card was: {self.cards[idx]}')
         if self.player.hilo == 'h':
-            if self.old_card > self.cards[idx]:
-                print(f'{self.old_card} is higher than {self.cards[idx]}.')
+            if self.cards[idx] > self.old_card:
+                print(f'{self.cards[idx]} is higher than {self.old_card}.')
                 self.player.score += 100
                 print(f'Your score is: {self.player.score}')
             else:
-                print(f'{self.old_card} is not higher than {self.cards[idx]}.')
+                print(f'{self.cards[idx]} is not higher than {self.old_card}.')
                 self.player.score -= 75
                 print(f'Your score is: {self.player.score}')
         elif self.player.hilo == 'l':
-            if self.old_card < self.cards[idx]:
-                print(f'{self.old_card} is lower than {self.cards[idx]}.')
+            if self.cards[idx] < self.old_card:
+                print(f'{self.cards[idx]} is lower than {self.old_card}.')
                 self.player.score += 100
                 print(f'Your score is: {self.player.score}')
             else:
-                print(f'{self.old_card} is not lower than {self.cards[idx]}.')
+                print(f'{self.cards[idx]} is not lower than {self.old_card}.')
                 self.player.score -= 75
                 print(f'Your score is: {self.player.score}')
-                
+        self.old_card = self.cards[idx]
+
     def do_outputs(self):
         """
         """
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,4 +1,4 @@
-class Thrower:
+class Player:
     """
     
     """
